b003b25158b5e4304d10d2462dd523a5e7eb1c9b
Markus Hitter <mah@jump-ing.de>
2019-05-04 14:16:41 +0200

    Add constructor entry 'tb_min_version'.
    This is a requirement introduced recently by the validation script.

--- dashtrends.php
+++ dashtrends.php
@@ -56,6 +56,7 @@ class Dashtrends extends Module
         $this->displayName = $this->l('Dashboard Trends');
         $this->description = $this->l('Adds a block with a graphical representation of the development of your store(s) based on selected key data.');
         $this->tb_versions_compliancy = '> 1.0.0';
+        $this->tb_min_version = '1.0.0';
     }
 
     /**
